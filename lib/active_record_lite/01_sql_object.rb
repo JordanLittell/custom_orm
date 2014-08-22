30661b04abdbe915f03355f4f7f3e15b7ce49f77
Jlit1001 <jordanlittell@yahoo.com>
2014-08-22 15:37:05 -0700

    wrote insert method

--- lib/active_record_lite/01_sql_object.rb
+++ lib/active_record_lite/01_sql_object.rb
@@ -23,7 +23,6 @@ class SQLObject
         self.attributes[col] = param 
       end
     end
-
   end
 
   def self.table_name=(table_name)
@@ -35,27 +34,64 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    query = "
+    SELECT 
+        #{self.table_name}.*
+    FROM 
+       #{self.table_name} 
+       "
+    result = DBConnection.execute(query)
+    self.parse_all(result)
   end
 
   def self.parse_all(results)
-    # ...
+    arr = []
+    results.each do |hash|
+      arr << self.new(hash)
+    end
+    arr
   end
 
   def self.find(id)
-    # ...
+    query = "
+    SELECT 
+        *
+    FROM 
+       #{self.table_name} 
+    WHERE 
+       id = #{id}
+       "
+    result = DBConnection.execute(query)
+    self.parse_all(result).first
   end
 
   def attributes
     @attributes ||= {}
   end
 
+
   def insert
-    # ...
+    col_names = @attributes.keys
+    str = ["?"]*col_names.length
+    col_names = col_names.join(",")
+    q_marks = str.join(", ")
+    result = DBConnection.execute(<<-SQL, *attribute_values)
+    INSERT INTO
+          #{self.class.table_name} (#{col_names})
+    VALUES
+          (#{q_marks})
+    SQL
+  
   end
 
-  def initialize
-    # ...
+  def initialize(params={})
+    params.each do |key,value| 
+      key = key.to_sym
+      unless self.class.columns.include?(key)
+        raise StandardError.new("unknown attribute '#{key}'") 
+      end
+      self.send("#{key}=".to_sym, value)
+    end
   end
 
   def save
@@ -67,6 +103,8 @@ class SQLObject
   end
 
   def attribute_values
-    # ...
+    # calling Array#map on SQLObject::columns, calling send on the instance to get the value.
+    attr_values = @attributes.keys.map{ |col_name| self.send(col_name) }
+    attr_values
   end
 end
