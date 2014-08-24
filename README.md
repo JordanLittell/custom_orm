e53715fe055763911249d005804f5295b5471013
Jlit1001 <jordanlittell@yahoo.com>
2014-08-24 10:54:26 -0700

    finished has_one through association

--- README.md
+++
@@ -1,3 +0,0 @@
-After the project, you can review the solution here:
-
-* https://github.com/appacademy-solutions/active_record_lite/tree/solution
--- lib/active_record_lite/04_associatable2.rb
+++ lib/active_record_lite/04_associatable2.rb
@@ -7,8 +7,23 @@ module Associatable
   def has_one_through(name, through_name, source_name)
     define_method "#{name}" do
     through_options = self.class.assoc_options[through_name] 
-   	class_options = through_options.model_class.assoc_options[source_name]
+   	source_options = through_options.model_class.assoc_options[source_name]
+   	source_table = "#{ source_options.table_name }"
+   	through_table = "#{ through_options.table_name}"
+   	first_belongs = self.send(through_options.class_name.to_s.downcase.to_sym)
+   	result = DBConnection.execute(<<-SQL, first_belongs.send(through_options.primary_key))
+   		SELECT #{ source_table }.* 
+   		FROM 
+   			#{through_table} 
+ 		JOIN 
+ 			#{source_table}
+ 		ON 
+ 			#{through_table}.#{ source_options.foreign_key } = #{source_table}.#{ source_options.primary_key }
+ 		WHERE 
+ 			#{ through_table }.id = ?
+   	SQL
+   	source_options.model_class.send(:new, result.first)
+   	
     end
   end
-  
 end
